2220a783ac213c55a6e227981e378e3a93496d7b
MelnikAnna <91954998+Anna122233234@users.noreply.github.com>
2022-04-17 00:14:31 +0300

    added task with ASCII picture

---
+++ Lesson_1/task_2/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "           ___" << endl
+         <<"            |     | |" << endl
+<<"           / \\    | |" << endl
+<<"          |--o|===|-|" << endl
+<<"          |---|   |d|" << endl
+<<"         /     \\  |w|" << endl
+<<"        | U     | |b|" << endl
+<<"        | S     |=| |" << endl
+<<"        | A     | | |" << endl
+<<"        |_______| |_|" << endl
+<<"         |@| |@|  | |" << endl
+<<"       ___________|_|_" << endl;
+    return 0;
+}
