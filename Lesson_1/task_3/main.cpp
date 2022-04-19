64e02fa9c81adb8ec0ae7f8c3e665fa26f813740
MelnikAnna <91954998+Anna122233234@users.noreply.github.com>
2022-04-19 22:00:54 +0300

    added changes to task with if/else operators

--- Lesson_1/task_3/main.cpp
+++ Lesson_1/task_3/main.cpp
@@ -4,15 +4,22 @@ using namespace std;
 
 int main()
 {
-    int motherboard, videocard, CPU, discount;
-    cout << "Enter motherboard price:" << endl;
-    cin >> motherboard;
-    cout << "Enter videocard price: " << endl;
-    cin >> videocard ;
-    cout << "Enter CPU price: " << endl;
-    cin >> CPU;
-    cout << "Enter discount: " << endl;
-    cin >> discount  ;
-    cout << "Your PC will cost " << motherboard + videocard + CPU ;
+    int age;
+    bool answer(true);
+    cout << "Enter your age:";
+    cin >> age;
+    if (age > 10){
+        cout << "Do you study?(1/0): " << endl;
+        cin >> answer;
+        if (answer){
+            cout <<"You are awesome!" << endl;
+        }
+        else {
+            cout << "Why?" << endl;
+        }
+    }
+    else {
+        cout << "You are realy big!";
+    }
     return 0;
 }
