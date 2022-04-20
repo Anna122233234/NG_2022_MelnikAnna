21e4b19044e73dc58c0df12ebb48c8c061096723
MelnikAnna <91954998+Anna122233234@users.noreply.github.com>
2022-04-20 23:36:12 +0300

    added changes to task added task with for statement

--- Lesson_2/task_1/main.cpp
+++ Lesson_2/task_1/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     int n;
-        cout << "Enter number : ";
+        cout << "Enter the number : ";
         cin >> n;
         for(int i = 0; i < n; i++)
         {
