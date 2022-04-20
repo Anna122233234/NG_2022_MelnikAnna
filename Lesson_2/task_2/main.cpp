043ec68546cb98a197f82c8c83fade2049e561ec
MelnikAnna <91954998+Anna122233234@users.noreply.github.com>
2022-04-20 23:40:35 +0300

    added changes to task with checking numbers for evenness

--- Lesson_2/task_2/main.cpp
+++ Lesson_2/task_2/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     int number, b = 0, sum;
-    cout << "Enter number: ";
+    cout << "Enter the number: ";
     cin >> number;
     while(number > 0){
         b = number % 10;
