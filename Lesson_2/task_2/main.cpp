e0aa0a38228ce3d20e1041eb439e3b17a0f3321f
MelnikAnna <91954998+Anna122233234@users.noreply.github.com>
2022-04-20 23:26:45 +0300

    added task with checking numbers for evenness

---
+++ Lesson_2/task_2/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number, b = 0, sum;
+    cout << "Enter number: ";
+    cin >> number;
+    while( number >0 ){
+        b = number % 10;
+        if(b%2==0) {
+            cout << b  << "; ";
+            sum += b;
+        }
+        number /= 10;
+    }
+    cout <<"\nSum = " << sum;
+    return 0;
+}
