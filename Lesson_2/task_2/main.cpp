b75a30de67f5e50546946d9b835a71d45ccbfe44
MelnikAnna <91954998+Anna122233234@users.noreply.github.com>
2022-04-26 17:20:05 +0300

    added several changes to task with finding roots of the equation

--- Lesson_2/task_2/main.cpp
+++ Lesson_2/task_2/main.cpp
@@ -5,14 +5,14 @@ using namespace std;
 int main()
 {
     int number, b = 0, sum;
-        cout << "Enter the number: ";
-        cin >> number;
-        for (int i = number; i > 0; i /=10){
-            if(i % 2 == 0) {
-                cout << i << "; " << endl;
-                sum +=i %10;
-            }
+    cout << "Enter the number: ";
+    cin >> number;
+    for (int i = number; i > 0; i /=10){
+        if(i % 2 == 0) {
+            cout << i << "; " << endl;
+            sum +=i %10;
         }
-        cout <<"\nSum = " << sum;
+    }
+    cout <<"\nSum = " << sum;
     return 0;
 }
