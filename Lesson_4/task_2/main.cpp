f83393d5ebbad52b2ec7197ae9d11dcc104c23b5
MelnikAnna <91954998+Anna122233234@users.noreply.github.com>
2022-05-13 16:49:05 +0300

    added changes to task with output of vowels in even indexes of a string

--- Lesson_4/task_2/main.cpp
+++ Lesson_4/task_2/main.cpp
@@ -4,10 +4,11 @@ using namespace std;
 
 int main()
 {
-    string str;
+    int size = 50;
+    char str[size];
     cout <<"Enter your string: "<< endl;
-    getline(cin, str);
-    for (int i = 0; i < str.length(); i++){
+    cin.getline(str, size);
+    for (int i = 0; i < size; i++){
         if ((i % 2 == 0 && str[i] == 'a') || (i % 2 == 0 && str[i] == 'i')||
             (i % 2 == 0 && str[i] == 'y') || (i % 2 == 0 && str[i] == 'e')||
             (i % 2 == 0 && str[i] == 'u') || (i % 2 == 0 && str[i] == 'o')){
