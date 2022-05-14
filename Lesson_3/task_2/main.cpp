579fc19447c101ee417841965d846b461e543d91
MelnikAnna <91954998+Anna122233234@users.noreply.github.com>
2022-05-14 19:56:18 +0300

    added changes to task with comparison of two numbers from the array

--- Lesson_3/task_2/main.cpp
+++ Lesson_3/task_2/main.cpp
@@ -7,31 +7,25 @@ int main()
 {
     srand(time(0));
     int array[4];
+    int sum, sum1 = 0;;
     cout << "It's the first array: ";
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < 4; i++){
         array [i] = 1 + rand () % 9;
-    for (int i = 0; i < 4; i++)
-        cout << array[i] << " ";
-    cout << endl;
-    cout << "It's the second array: ";
+        cout << array[i];
+    }
+    cout << endl << "It's the second array: ";
     int array1[4];
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < 4; i++){
         array1 [i] = 1 + rand () % 9;
-    for (int i = 0; i < 4; i++)
-        cout << array1[i] << " ";
-    int sum = 0;
-    int sum1 = 0;
+        cout << array1[i];
+    }
     sum = array[0]*1000 + array[1]*100 + array[2]*10 + array[3];
     sum1 = array1[0]*1000 + array1[1]*100 + array1[2]*10 + array1[3];
-    cout << endl;
-    if (sum > sum1){
-        cout << sum << " > " << sum1;
-    }
-    else if (sum < sum1){
-        cout << sum << " < " << sum1;
-    }
-    else if (sum == sum1){
-        cout << sum << " = " << sum1;
-    }
+    if (sum > sum1)
+        cout << endl << sum << " > " << sum1;
+    else if (sum < sum1)
+        cout << endl << sum << " < " << sum1;
+    else if (sum == sum1)
+        cout << endl << sum << " = " << sum1;
     return 0;
 }
