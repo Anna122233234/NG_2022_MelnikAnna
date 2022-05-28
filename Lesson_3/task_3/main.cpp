148aa1865cd4c8b0bbd64b2b6e88db3cab891802
MelnikAnna <91954998+Anna122233234@users.noreply.github.com>
2022-05-29 00:28:50 +0300

    added changes to tabulation to task with output of stars vertically

--- Lesson_3/task_3/main.cpp
+++ Lesson_3/task_3/main.cpp
@@ -4,33 +4,32 @@ using namespace std;
 
 int main()
 {
-   int array[20];
-   int count = 0;
-   int m = 0;
-   for (int i= 0; i < 20; i++){
-           cout << "[" << i + 1 << "]" << ": ";
-           cin >> array [i];
-           if (array [i] == 0){
-           count = i;
-           break;
-           }
-           if (m < array [i]){
-               m = array [i];
-           }
-       }
-   for(int i =0; i< m; i++){
-       for (int j =0; j < count; j++){
-           if (array [j] > 0 ){
-               cout << "*";
-               array[j] -= 1;
-           }
+    int array[20];
+    int count = 0;
+    int m = 0;
+    for (int i= 0; i < 20; i++){
+        cout << "[" << i + 1 << "]" << ": ";
+        cin >> array [i];
+        if (array [i] == 0){
+            count = i;
+            break;
+        }
+        if (m < array [i]){
+            m = array [i];
+        }
+    }
+    for(int i =0; i< m; i++){
+        for (int j =0; j < count; j++){
+            if (array [j] > 0 ){
+                cout << "*";
+                array[j] -= 1;
+            }
             else{
-               cout << " ";
-               array[j] -= 1;
-           }
-       }
-    cout << endl;
+                cout << " ";
+                array[j] -= 1;
+            }
+        }
+        cout << endl;
    }
-
 }
 
