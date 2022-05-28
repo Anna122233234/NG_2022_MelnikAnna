0ade401c53a519ba26681705ade00fd2efd1aa7c
MelnikAnna <91954998+Anna122233234@users.noreply.github.com>
2022-05-29 01:14:26 +0300

    added changes to initializer function

--- Lesson_5/task_4/main.cpp
+++ Lesson_5/task_4/main.cpp
@@ -5,25 +5,17 @@ using namespace std;
 
 void initializer(){
     float a, b, c, D;
-    cout << "Enter a: ";
-    cin >> a;
-    cout << "Enter b: ";
-    cin >> b;
-    cout << "Enter c: ";
-    cin >> c;
-        D = b * b - (4* a * c);
-        cout <<"D = "<< D << endl;
-        if (D > 0){
-            D = sqrt(D);
-            cout << "x1 = " << (-b - D)/ (2*a) << endl;
-            cout << "x2 = " << (-b + D)/ (2*a) << endl;
-        }
-        if (D == 0){
-            cout <<"x = " << (-b - D)/ (2*a) << endl;
-        }
-        if (D < 0) {
-            cout <<"There are no roots " << endl;
-        }
+    cout << "Enter a: " << a << endl
+         << "Enter b: " << b << endl
+         << "Enter c: " << c << endl;
+    D = b * b - (4* a * c);
+    cout <<"D = "<< D << endl;
+    if(D > 0){
+        D = sqrt(D);
+        cout << "x1 = " << (-b - D)/ (2*a) << endl <<"x2 = " << (-b + D)/ (2*a) << endl;
+    }
+    if(D == 0){cout <<"x = " << (-b - D)/ (2*a) << endl;}
+    if (D < 0){cout <<"There are no roots " << endl;}
 }
 int main()
 {
