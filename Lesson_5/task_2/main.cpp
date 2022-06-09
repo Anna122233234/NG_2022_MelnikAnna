5680b746791b86c6e6d57ee3d41621831b433a40
MelnikAnna <91954998+Anna122233234@users.noreply.github.com>
2022-06-09 23:26:28 +0300

    added task with finding the number in the array

---
+++ Lesson_5/task_2/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int farray(int array[], int i){
+    if(i < 5){
+        cout << array[i] << " ";
+        return farray(array, i+1);
+    }
+}
+int findnumber(int array[], int key, int i){
+    if (i < 5){
+        if (array[i] == key){
+            cout << "This number is in the array: ";
+        }
+        return findnumber(array, key, i+1);
+    }
+
+}
+
+int main()
+{
+    int array[5]= {5, 8, 3, 9, 1};
+    int key;
+    cout << "It`s your array: " << endl;
+    cout << farray(array,0);
+    cout << endl << "Enter the number: ";
+    cin  >> key;
+    cout << findnumber(array, key, 0);
+    return 0;
+}
