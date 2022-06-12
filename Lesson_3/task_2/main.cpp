1882e792bd060b05c0b0d951d5b68e817580e556
MelnikAnna <91954998+Anna122233234@users.noreply.github.com>
2022-06-12 22:01:49 +0300

    added changes to array to task with comparison of two numbers

--- Lesson_3/task_2/main.cpp
+++ Lesson_3/task_2/main.cpp
@@ -7,17 +7,11 @@ int main()
 {
     srand(time(0));
     int array[4];
-    int sum, sum1 = 0;;
-    cout << "It's the first array: ";
+    int array1[4];
+    int sum, sum1 = 0;
     for (int i = 0; i < 4; i++){
         array [i] = 1 + rand () % 9;
-        cout << array[i];
-    }
-    cout << endl << "It's the second array: ";
-    int array1[4];
-    for (int i = 0; i < 4; i++){
         array1 [i] = 1 + rand () % 9;
-        cout << array1[i];
     }
     int res, res1 = 0, i, pow;
     for (i = 3, pow = 1; i>=0; --i, pow*=10) {
